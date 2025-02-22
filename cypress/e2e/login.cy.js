39156ab253067d6b24571198fb5e5f454e47edb6
Ikha Setiawan <you@example.com>
2025-02-22 21:57:03 +0700

    Verify Functional Login Page

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,41 @@
 describe('Magento Login Page', () => {
   it('Login Success', () => {
-    cy.visit('https://magento.softwaretestingboard.com/')
+    cy.visit('https://magento.softwaretestingboard.com/', { timeout: 120000, failOnStatusCode: false })
+    cy.wait(5000)
     cy.get('.panel > .header > .authorization-link > a').click()
     cy.get('#email').type('sibunavi@cyclelove.cc')
     cy.get('#pass').type('Demo1234')
     cy.get('#send2').click()
+    cy.get('.logo > img').should('be.visible')
   })
 })
 it('Login Empty Email', () => {
-  cy.get('.panel > .header > .authorization-link > a').click()
-  cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type('Demo1234')
-  cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2').click()
-})
+  cy.visit('https://magento.softwaretestingboard.com/customer/account/login/')
+    cy.get('#email').clear()
+    cy.get('#pass').type('Demo1234')
+    cy.get('#send2').click()
+
+    cy.get('#email-error').should('be.visible')
+  })
+  it('Login Empty Password', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/login/', { timeout: 60000 }) 
+
+    cy.get('#email').type('sibunavi@cyclelove.cc')
+    cy.get('#pass').clear()
+    cy.get('#send2').click()
+
+    cy.get('#pass-error', { timeout: 10000 }).should('be.visible')
+  })
+  it('Login Empty Email and Password'), () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/login/', { timeout: 60000 }) 
+
+    cy.get('#email').clear()
+    cy.get('#pass').clear()
+    cy.get('#send2').click()
+
+    cy.get('#email-error', { timeout: 10000 }).should('be.visible')
+    .and('contain', 'This is a required field.')
+  
+  cy.get('#pass-error', { timeout: 10000 }).should('be.visible')
+    .and('contain', 'This is a required field.')  
+  }
